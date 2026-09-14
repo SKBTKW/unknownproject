aa2cf82bf81dfef37344e29dbbbe11a9311263ba
SKBTKW <skbtkw0814@gmail.com>
2026-09-14 20:20:45 +0900

    warning: expose investigation foundation exports

---
+++ game/src/warning/index.js
@@ -0,0 +1,3 @@
+export { createKnownEnemyState, recordInvestigationReport } from './domain/known_enemy_state.js';
+export { createInvestigationReport } from './domain/investigation_report.js';
+export { createObservableEnemyProfile } from './domain/observable_enemy_profile.js';
