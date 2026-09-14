f92edbda7a2a4b0f2bd2ebe92f6e2d83643a4109
SKBTKW <skbtkw0814@gmail.com>
2026-09-14 20:52:22 +0900

    Export investigation resolver

--- game/src/warning/index.js
+++ game/src/warning/index.js
@@ -2,3 +2,4 @@ export { createKnownEnemyState, recordInvestigationReport } from './domain/known
 export { createInvestigationReport } from './domain/investigation_report.js';
 export { createObservableEnemyProfile } from './domain/observable_enemy_profile.js';
 export { EnemyObservationProjector } from './systems/enemy_observation_projector.js';
+export { InvestigationResolver } from './systems/investigation_resolver.js';
