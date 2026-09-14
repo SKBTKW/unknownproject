46feb687d35b29998dac99a139e62365ce6a7ea6
SKBTKW <skbtkw0814@gmail.com>
2026-09-14 20:28:22 +0900

    Export enemy observation projector

--- game/src/warning/index.js
+++ game/src/warning/index.js
@@ -1,3 +1,4 @@
 export { createKnownEnemyState, recordInvestigationReport } from './domain/known_enemy_state.js';
 export { createInvestigationReport } from './domain/investigation_report.js';
 export { createObservableEnemyProfile } from './domain/observable_enemy_profile.js';
+export { EnemyObservationProjector, DEFAULT_SCALE_BANDS } from './systems/enemy_observation_projector.js';
