f2250dbc2626ce85538532c8deb9662184769983
SKBTKW <skbtkw0814@gmail.com>
2026-09-16 13:23:40 +0900

    feat(warning): export investigation warning bridge

--- game/src/warning/index.js
+++ game/src/warning/index.js
@@ -18,6 +18,7 @@ export { InvestigationCardExecutionService } from './systems/investigation_card_
 export { InvestigationUnlockBridge, DEFAULT_UNLOCK_EVENT_IDS } from './systems/investigation_unlock_bridge.js';
 export { WarningStateService } from './systems/warning_state_service.js';
 export { WarningOmenBridge, DEFAULT_OMEN_EVENT_IDS } from './systems/warning_omen_bridge.js';
+export { WarningInvestigationBridge } from './systems/warning_investigation_bridge.js';
 export { WarningSettlementBridge } from './systems/warning_settlement_bridge.js';
 export { attachInvestigationRuntime } from './integration/investigation_runtime_bridge.js';
 export { attachInvestigationSubsystem } from './integration/investigation_bootstrap.js';
